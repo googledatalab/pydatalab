024ad3c4e35e2b528b8a9087b7b5dfaffbb9edb6
Yasser Elsayed <yelsayed@google.com>
2016-09-28 14:37:32 -0700

    fix merge conflict

--- datalab/stackdriver/commands/__init__.py
+++ datalab/stackdriver/commands/__init__.py
@@ -10,24 +10,7 @@
 # or implied. See the License for the specific language governing permissions and limitations under
 # the License.
 
-"""CloudML Helper Library."""
 
 from __future__ import absolute_import
+
 from . import _monitoring
-from ._local_runner import LocalRunner
-from ._cloud_runner import CloudRunner
-from ._metadata import Metadata
-from ._local_predictor import LocalPredictor
-from ._cloud_predictor import CloudPredictor
-from ._job import Jobs
-from ._summary import Summary
-from ._tensorboard import TensorBoardManager
-from ._dataset import DataSet
-from ._package import Packager
-from ._cloud_models import CloudModels, CloudModelVersions
-from ._confusion_matrix import ConfusionMatrix
-
-from plotly.offline import init_notebook_mode
-
-init_notebook_mode()
-
