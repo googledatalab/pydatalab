281d252d6c839a1b70082715a7db19f3f5e365a0
Supriya Garg <supriyagarg@users.noreply.github.com>
2016-09-28 12:51:12 -0400

    Add a QueryMetadata class to the stackdriver monitoring module (#64)
    * Rename _timeseries.py to _query.py
    
    * Add the QueryMetadata class.
    
    The `labels_as_dataframe` gets removed from the Query class, and the
    functionality is moved to the QueryMetadata class.
    
    You can get the metadata of a query by calling the method `metadata()`.
    
    * Add unit tests for QueryMetadata.
    
    * Add QueryMetadata to the Sphinx docs.
    
    * Drop unused imports

--- datalab/stackdriver/monitoring/__init__.py
+++ datalab/stackdriver/monitoring/__init__.py
@@ -16,5 +16,6 @@ from __future__ import absolute_import
 
 from google.cloud.monitoring import Aligner, Reducer
 from ._metric import MetricDescriptors
+from ._query import Query
+from ._query_metadata import QueryMetadata
 from ._resource import ResourceDescriptors
-from ._timeseries import Query
