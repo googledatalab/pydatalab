579448c97666ef5acc262908f5e97de8802ccbc5
Yasser Elsayed <yebrahim@users.noreply.github.com>
2017-02-06 09:56:57 -0800

    Add tables and datasets subcommands of %bq, rework create, list, delete, and describe into them (#162)

--- google/datalab/bigquery/commands/_bigquery.py
+++ google/datalab/bigquery/commands/_bigquery.py
@@ -37,35 +37,61 @@ import google.datalab.utils
 import google.datalab.utils.commands
 
 
-def _create_create_subparser(parser):
-  create_parser = parser.subcommand('create', 'Create a dataset or table.')
-  sub_commands = create_parser.add_subparsers(dest='command')
+def _create_dataset_subparser(parser):
+  dataset_parser = parser.subcommand('datasets', 'Operations on BigQuery datasets')
+  sub_commands = dataset_parser.add_subparsers(dest='command')
 
-  create_dataset_parser = sub_commands.add_parser('dataset', help='Create a dataset.')
-  create_dataset_parser.add_argument('-n', '--name', help='The name of the dataset to create.',
+  # %%bq datasets list
+  list_parser = sub_commands.add_parser('list', help='List datasets')
+  list_parser.add_argument('-p', '--project',
+                           help='The project whose datasets should be listed')
+  list_parser.add_argument('-f', '--filter',
+                           help='Optional wildcard filter string used to limit the results')
+
+  # %%bq datasets create
+  create_parser = sub_commands.add_parser('create', help='Create a dataset.')
+  create_parser.add_argument('-n', '--name', help='The name of the dataset to create.',
                                      required=True)
-  create_dataset_parser.add_argument('-f', '--friendly', help='The friendly name of the dataset.')
+  create_parser.add_argument('-f', '--friendly', help='The friendly name of the dataset.')
 
-  create_table_parser = sub_commands.add_parser('table', help='Create a table.')
-  create_table_parser.add_argument('-n', '--name', help='The name of the table to create.',
-                                   required=True)
-  create_table_parser.add_argument('-o', '--overwrite', help='Overwrite table if it exists.',
-                                   action='store_true')
-  return create_parser
-
-
-def _create_delete_subparser(parser):
-  delete_parser = parser.subcommand('delete', 'Delete a dataset or table.')
-  sub_commands = delete_parser.add_subparsers(dest='command')
-
-  delete_dataset_parser = sub_commands.add_parser('dataset', help='Delete a dataset.')
+  # %%bq datasets delete
+  delete_dataset_parser = sub_commands.add_parser('delete', help='Delete a dataset.')
   delete_dataset_parser.add_argument('-n', '--name', help='The name of the dataset to delete.',
                                      required=True)
 
-  delete_table_parser = sub_commands.add_parser('table', help='Delete a table.')
-  delete_table_parser.add_argument('-n', '--name', help='The name of the table to delete.',
+  return dataset_parser
+
+
+def _create_table_subparser(parser):
+  table_parser = parser.subcommand('tables', 'Operations on BigQuery tables')
+  sub_commands = table_parser.add_subparsers(dest='command')
+
+  # %%bq tables list
+  list_parser = sub_commands.add_parser('list', help='List the tables in a BigQuery project or dataset.')
+  list_parser.add_argument('-p', '--project',
+                             help='The project whose tables should be listed')
+  list_parser.add_argument('-d', '--dataset',
+                             help='The dataset to restrict to')
+  list_parser.add_argument('-f', '--filter',
+                             help='Optional wildcard filter string used to limit the results')
+
+  # %%bq tables create
+  create_parser = sub_commands.add_parser('create', help='Create a table.')
+  create_parser.add_argument('-n', '--name', help='The name of the table to create.',
                                    required=True)
-  return delete_parser
+  create_parser.add_argument('-o', '--overwrite', help='Overwrite table if it exists.',
+                                   action='store_true')
+
+  # %%bq tables describe
+  describe_parser = sub_commands.add_parser('describe', help='View a table\'s schema')
+  describe_parser.add_argument('-n', '--name', help='Name of table to show', required=True)
+
+  # %%bq tables delete
+  delete_parser = sub_commands.add_parser('delete', help='Delete a table.')
+  delete_parser.add_argument('-n', '--name', help='The name of the table to delete.',
+                                   required=True)
+
+  return table_parser
 
 
 def _create_sample_subparser(parser):
@@ -172,44 +198,6 @@ def _create_pipeline_subparser(parser):
   # TODO(gram): we may want to move some command line arguments to the cell body config spec
   # eventually.
   return pipeline_parser
-
-
-def _create_table_subparser(parser):
-  table_parser = parser.subcommand('table', 'View a BigQuery table.')
-  table_parser.add_argument('-r', '--rows', type=int, default=25,
-                            help='Rows to display per page')
-  table_parser.add_argument('-c', '--cols',
-                            help='Comma-separated list of column names to restrict to')
-  table_parser.add_argument('table', help='The name of, or a reference to, the table or view')
-  return table_parser
-
-
-def _create_schema_subparser(parser):
-  schema_parser = parser.subcommand('schema', 'View a BigQuery table or view schema.')
-  group = schema_parser.add_mutually_exclusive_group()
-  group.add_argument('-v', '--view', help='the name of the view whose schema should be displayed')
-  group.add_argument('-t', '--table', help='the name of the table whose schema should be displayed')
-  return schema_parser
-
-
-def _create_datasets_subparser(parser):
-  datasets_parser = parser.subcommand('datasets', 'List the datasets in a BigQuery project.')
-  datasets_parser.add_argument('-p', '--project',
-                               help='The project whose datasets should be listed')
-  datasets_parser.add_argument('-f', '--filter',
-                               help='Optional wildcard filter string used to limit the results')
-  return datasets_parser
-
-
-def _create_tables_subparser(parser):
-  tables_parser = parser.subcommand('tables', 'List the tables in a BigQuery project or dataset.')
-  tables_parser.add_argument('-p', '--project',
-                             help='The project whose tables should be listed')
-  tables_parser.add_argument('-d', '--dataset',
-                             help='The dataset to restrict to')
-  tables_parser.add_argument('-f', '--filter',
-                             help='Optional wildcard filter string used to limit the results')
-  return tables_parser
 
 
 def _create_extract_subparser(parser):
@@ -357,68 +345,6 @@ def _sample_cell(args, cell_body):
     return google.datalab.utils.commands.profile_df(results.to_dataframe())
   else:
     return results
-
-
-def _create_cell(args, cell_body):
-  """Implements the BigQuery cell magic used to create datasets and tables.
-
-   The supported syntax is:
-
-     %%bq create dataset -n|--name <name> [-f|--friendly <friendlyname>]
-     [<description>]
-
-   or:
-
-     %%bq create table -n|--name <tablename> [--overwrite]
-     [<YAML or JSON cell_body defining schema to use for tables>]
-
-  Args:
-    args: the argument following '%bq create <command>'.
-  """
-  if args['command'] == 'dataset':
-    try:
-      google.datalab.bigquery.Dataset(args['name']).create(friendly_name=args['friendly'],
-                                                    description=cell_body)
-    except Exception as e:
-      print('Failed to create dataset %s: %s' % (args['name'], e))
-  else:
-    if cell_body is None:
-      print('Failed to create %s: no schema specified' % args['name'])
-    else:
-      try:
-        record = google.datalab.utils.commands.parse_config(cell_body, google.datalab.utils.commands.notebook_environment(), as_dict=False)
-        schema = google.datalab.bigquery.Schema(record)
-        google.datalab.bigquery.Table(args['name']).create(schema=schema, overwrite=args['overwrite'])
-      except Exception as e:
-        print('Failed to create table %s: %s' % (args['name'], e))
-
-
-def _delete_cell(args, _):
-  """Implements the BigQuery cell magic used to delete datasets and tables.
-
-   The supported syntax is:
-
-     %%bq delete dataset -n|--name <name>
-
-   or:
-
-     %%bq delete table -n|--name <name>
-
-  Args:
-    args: the argument following '%bq delete <command>'.
-  """
-  # TODO(gram): add support for wildchars and multiple arguments at some point. The latter is
-  # easy, the former a bit more tricky if non-default projects are involved.
-  if args['command'] == 'dataset':
-    try:
-      google.datalab.bigquery.Dataset(args['name']).delete()
-    except Exception as e:
-      print('Failed to delete dataset %s: %s' % (args['name'], e))
-  else:
-    try:
-      google.datalab.bigquery.Table(args['name']).delete()
-    except Exception as e:
-      print('Failed to delete table %s: %s' % (args['name'], e))
 
 
 def _dryrun_cell(args, cell_body):
@@ -580,29 +506,6 @@ def _pipeline_cell(args, cell_body):
     query.execute(output_options, dialect=args['dialect'], billing_tier=args['billing']).results
 
 
-def _table_line(args):
-  """Implements the BigQuery table magic used to display tables.
-
-   The supported syntax is:
-   %bq table -t|--table <name> <other args>
-
-  Args:
-    args: the arguments following '%bq table'.
-  Returns:
-    The HTML rendering for the table.
-  """
-  # TODO(gram): It would be good to turn _table_viewer into a class that has a registered
-  # renderer. That would allow this to return a table viewer object which is easier to test.
-  name = args['table']
-  table = _get_table(name)
-  if table and table.exists():
-    fields = args['cols'].split(',') if args['cols'] else None
-    html = _table_viewer(table, rows_per_page=args['rows'], fields=fields)
-    return IPython.core.display.HTML(html)
-  else:
-    raise Exception('Table %s does not exist; cannot display' % name)
-
-
 def _get_schema(name):
   """ Given a variable or table name, get the Schema if it exists. """
   item = google.datalab.utils.commands.get_notebook_item(name)
@@ -644,27 +547,6 @@ def _get_table(name):
   return None
 
 
-def _schema_line(args):
-  """Implements the BigQuery schema magic used to display table/view schemas.
-
-  Args:
-    args: the arguments following '%bq schema'.
-  Returns:
-    The HTML rendering for the schema.
-  """
-  # TODO(gram): surely we could just return the schema itself?
-  name = args['table'] if args['table'] else args['view']
-  if name is None:
-    raise Exception('No table or view specified; cannot show schema')
-
-  schema = _get_schema(name)
-  if schema:
-    html = _repr_html_table_schema(schema)
-    return IPython.core.display.HTML(html)
-  else:
-    raise Exception('%s is not a schema and does not appear to have a schema member' % name)
-
-
 def _render_table(data, fields=None):
   """ Helper to render a list of dictionaries as an HTML display object. """
   return IPython.core.display.HTML(google.datalab.utils.commands.HtmlBuilder.render_table(data, fields))
@@ -675,49 +557,96 @@ def _render_list(data):
   return IPython.core.display.HTML(google.datalab.utils.commands.HtmlBuilder.render_list(data))
 
 
-def _datasets_line(args):
-  """Implements the BigQuery datasets magic used to display datasets in a project.
+def _dataset_line(args):
+  """Implements the BigQuery dataset magic subcommand used to operate on datasets
 
    The supported syntax is:
+   %bq datasets <command> <args>
 
-       %bq datasets [-f <filter>] [-p|--project <project_id>]
+  Commands:
+    {list, create, delete}
 
   Args:
-    args: the arguments following '%bq datasets'.
+    args: the optional arguments following '%bq datasets command'.
+  """
+  if args['command'] == 'list':
+    filter_ = args['filter'] if args['filter'] else '*'
+    return _render_list([str(dataset) for dataset in google.datalab.bigquery.Datasets(args['project'])
+                         if fnmatch.fnmatch(str(dataset), filter_)])
+
+  elif args['command'] == 'create':
+    try:
+      google.datalab.bigquery.Dataset(args['name']).create(friendly_name=args['friendly'],
+                                                    description=cell_body)
+    except Exception as e:
+      print('Failed to create dataset %s: %s' % (args['name'], e))
+
+  elif args['command'] == 'delete':
+    try:
+      google.datalab.bigquery.Dataset(args['name']).delete()
+    except Exception as e:
+      print('Failed to delete dataset %s: %s' % (args['name'], e))
+
+
+def _table_cell(args, cell_body):
+  """Implements the BigQuery table magic subcommand used to operate on tables
+
+   The supported syntax is:
+   %%bq tables <command> <args>
+
+  Commands:
+    {list, create, delete}
+
+  Args:
+    args: the optional arguments following '%%bq tables command'.
+    cell_body: optional contents of the cell interpreted as SQL, YAML or JSON.
   Returns:
     The HTML rendering for the table of datasets.
   """
-  filter_ = args['filter'] if args['filter'] else '*'
-  return _render_list([str(dataset) for dataset in google.datalab.bigquery.Datasets(args['project'])
-                       if fnmatch.fnmatch(str(dataset), filter_)])
-
-
-def _tables_line(args):
-  """Implements the BigQuery tables magic used to display tables in a dataset.
-
-   The supported syntax is:
-
-       %bq tables -p|--project <project_id>  -d|--dataset <dataset_id>
-
-  Args:
-    args: the arguments following '%bq tables'.
-  Returns:
-    The HTML rendering for the list of tables.
-  """
-  filter_ = args['filter'] if args['filter'] else '*'
-  if args['dataset']:
-    if args['project'] is None:
-      datasets = [google.datalab.bigquery.Dataset(args['dataset'])]
+  if args['command'] == 'list':
+    filter_ = args['filter'] if args['filter'] else '*'
+    if args['dataset']:
+      if args['project'] is None:
+        datasets = [google.datalab.bigquery.Dataset(args['dataset'])]
+      else:
+        datasets = [google.datalab.bigquery.Dataset((args['project'], args['dataset']))]
     else:
-      datasets = [google.datalab.bigquery.Dataset((args['project'], args['dataset']))]
-  else:
-    datasets = google.datalab.bigquery.Datasets(args['project'])
+      datasets = google.datalab.bigquery.Datasets(args['project'])
 
-  tables = []
-  for dataset in datasets:
-    tables.extend([str(table) for table in dataset if fnmatch.fnmatch(str(table), filter_)])
+    tables = []
+    for dataset in datasets:
+      tables.extend([str(table) for table in dataset if fnmatch.fnmatch(str(table), filter_)])
 
-  return _render_list(tables)
+    return _render_list(tables)
+
+  elif args['command'] == 'create':
+    if cell_body is None:
+      print('Failed to create %s: no schema specified' % args['name'])
+    else:
+      try:
+        record = google.datalab.utils.commands.parse_config(cell_body,
+                                         google.datalab.utils.commands.notebook_environment(),
+                                         as_dict=False)
+        schema = google.datalab.bigquery.Schema(record)
+        google.datalab.bigquery.Table(args['name']).create(schema=schema,
+                                                    overwrite=args['overwrite'])
+      except Exception as e:
+        print('Failed to create table %s: %s' % (args['name'], e))
+
+  elif args['command'] == 'describe':
+    name = args['name']
+    table = _get_table(name)
+    if not table:
+      raise Exception('Could not find table %s' % name)
+
+    html = _repr_html_table_schema(table.schema)
+    return IPython.core.display.HTML(html)
+
+  elif args['command'] == 'delete':
+    try:
+      google.datalab.bigquery.Table(args['name']).delete()
+    except Exception as e:
+      print('Failed to delete table %s: %s' % (args['name'], e))
 
 
 def _extract_cell(args, cell_body):
@@ -827,14 +756,20 @@ for help on a specific command.
   # This is a bit kludgy because we want to handle some line magics and some cell magics
   # with the bq command.
 
+  # %bq datasets
+  _add_command(parser, _create_dataset_subparser, _dataset_line, cell_prohibited=True)
+
+  # %bq tables
+  _add_command(parser, _create_table_subparser, _table_cell)
+
+  # %%bq execute
+  _add_command(parser, _create_execute_subparser, _execute_cell)
+
+  # %bq extract
+  _add_command(parser, _create_extract_subparser, _extract_cell)
+
   # %%bq sample
   _add_command(parser, _create_sample_subparser, _sample_cell)
-
-  # %%bq create
-  _add_command(parser, _create_create_subparser, _create_cell)
-
-  # %%bq delete
-  _add_command(parser, _create_delete_subparser, _delete_cell)
 
   # %%bq dryrun
   _add_command(parser, _create_dryrun_subparser, _dryrun_cell)
@@ -842,26 +777,8 @@ for help on a specific command.
   # %%bq udf
   _add_command(parser, _create_udf_subparser, _udf_cell, cell_required=True)
 
-  # %%bq execute
-  _add_command(parser, _create_execute_subparser, _execute_cell)
-
   # %%bq pipeline
   _add_command(parser, _create_pipeline_subparser, _pipeline_cell)
-
-  # %bq table
-  _add_command(parser, _create_table_subparser, _table_line, cell_prohibited=True)
-
-  # %bq schema
-  _add_command(parser, _create_schema_subparser, _schema_line, cell_prohibited=True)
-
-  # %bq datasets
-  _add_command(parser, _create_datasets_subparser, _datasets_line, cell_prohibited=True)
-
-  # %bq tables
-  _add_command(parser, _create_tables_subparser, _tables_line, cell_prohibited=True)
-
-  # %bq extract
-  _add_command(parser, _create_extract_subparser, _extract_cell)
 
   # %bq load
   # TODO(gram): need some additional help, esp. around the option of specifying schema in
