991e92c0686eea032be2c8726c0296d691002087
Brandon Dutra <brandondutra@google.com>
2017-05-10 19:52:01 -0700

    make code_free_ml package work with dataflow (#372)
    * sw
    
    * Transfrom scrpt done. Includes image support.
    
    * make transform work with dataflow
    
    * review comments
    
    * sw
    
    * sw
    
    * sw
    
    * cloud dataflow working, tests updated
    
    * flake8
    
    * tried to fix gh history, didn't work, so just delete old files
    
    * small change
    
    * flake8

---
+++ solutionbox/code_free_ml/mltoolbox/code_free_ml/data/setup.py
@@ -0,0 +1,38 @@
+# Copyright 2017 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing permissions and limitations under
+# the License.
+
+# This setup file is used when running cloud training or cloud dataflow jobs.
+from setuptools import setup, find_packages
+
+
+setup(
+  name='trainer',
+  version='1.0.0',
+  packages=find_packages(),
+  description='Google Cloud Datalab helper sub-package',
+  author='Google',
+  author_email='google-cloud-datalab-feedback@googlegroups.com',
+  keywords=[
+  ],
+  license="Apache Software License",
+  long_description="""
+  """,
+  install_requires=[
+    'google-cloud-dataflow==0.6.0',
+    'tensorflow-transform==0.1.7',
+    'tensorflow==1.0.1',
+    'protobuf==3.1.0',
+  ],
+  package_data={
+  },
+  data_files=[],
+)
