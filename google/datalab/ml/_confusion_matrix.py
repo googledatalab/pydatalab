bf0eee10b5fa740b204a8a9ff99f671cbba623a2
qimingj <qimingj@users.noreply.github.com>
2017-07-18 16:41:18 -0700

    Fix confusion matrix when datasource is BigQuery. (#463)
    * Fix confusion matrix when datasource is BigQuery.
    
    * CR comments.

--- google/datalab/ml/_confusion_matrix.py
+++ google/datalab/ml/_confusion_matrix.py
@@ -121,10 +121,18 @@ class ConfusionMatrix(object):
     tick_marks = np.arange(len(self._labels))
     plt.xticks(tick_marks, self._labels, rotation=45)
     plt.yticks(tick_marks, self._labels)
-    thresh = self._cm.max() / 2.
-    for i, j in itertools.product(range(self._cm.shape[0]), range(self._cm.shape[1])):
-      plt.text(j, i, self._cm[i, j], horizontalalignment="center",
-               color="white" if self._cm[i, j] > thresh else "black")
+    if isinstance(self._cm, list):
+      # If cm is created from BigQuery then it is a list.
+      thresh = max(max(self._cm)) / 2.
+      for i, j in itertools.product(range(len(self._labels)), range(len(self._labels))):
+        plt.text(j, i, self._cm[i][j], horizontalalignment="center",
+                 color="white" if self._cm[i][j] > thresh else "black")
+    else:
+      # If cm is created from csv then it is a sklearn's confusion_matrix.
+      thresh = self._cm.max() / 2.
+      for i, j in itertools.product(range(len(self._labels)), range(len(self._labels))):
+        plt.text(j, i, self._cm[i, j], horizontalalignment="center",
+                 color="white" if self._cm[i, j] > thresh else "black")
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
