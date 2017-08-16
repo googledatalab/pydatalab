83de4580f1c3ffbbe40e5c41ecc09044d10f8eb2
qimingj <qimingj@users.noreply.github.com>
2017-08-16 10:09:00 -0700

    Fix facets issues. (#477)
    - FacetsOverview, need to clean up non-ascii.
    - FacetsDiveview, handle json data correctly.

--- google/datalab/ml/_fasets.py
+++ google/datalab/ml/_fasets.py
@@ -15,12 +15,25 @@ import base64
 import google.datalab as datalab
 from google.datalab.utils.facets.generic_feature_statistics_generator \
     import GenericFeatureStatisticsGenerator
+import numpy as np
 import pandas as pd
+import re
 import six
 
 
 class FacetsOverview(object):
   """Represents A facets overview. """
+
+  def _remove_nonascii(self, df):
+    """Make copy and remove non-ascii characters from it."""
+
+    df_copy = df.copy(deep=True)
+    for col in df_copy.columns:
+      if (df_copy[col].dtype == np.dtype('O')):
+        df_copy[col] = df[col].apply(
+          lambda x: re.sub(r'[^\x00-\x7f]', r'', x) if isinstance(x, six.string_types) else x)
+
+    return df_copy
 
   def plot(self, data):
     """ Plots an overview in a list of dataframes
@@ -35,7 +48,7 @@ class FacetsOverview(object):
       raise ValueError('Expect a dictionary where the values are all dataframes.')
 
     gfsg = GenericFeatureStatisticsGenerator()
-    data = [{'name': k, 'table': v} for k, v in six.iteritems(data)]
+    data = [{'name': k, 'table': self._remove_nonascii(v)} for k, v in six.iteritems(data)]
     data_proto = gfsg.ProtoFromDataFrames(data)
     protostr = base64.b64encode(data_proto.SerializeToString()).decode("utf-8")
     html_id = 'f' + datalab.utils.commands.Html.next_id()
@@ -65,14 +78,12 @@ class FacetsDiveview(object):
       raise ValueError('Expect a DataFrame.')
 
     jsonstr = data.to_json(orient='records')
-    # Escape ' and " because the string will be in <script> block.
-    jsonstr = jsonstr.replace('\'', '\\\'').replace('\\"', '\\\\\\"')
     html_id = 'f' + datalab.utils.commands.Html.next_id()
     HTML_TEMPLATE = """
         <link rel="import" href="/nbextensions/gcpdatalab/extern/facets-jupyter.html">
         <facets-dive id="{html_id}" height="600"></facets-dive>
         <script>
-          var data = JSON.parse('{jsonstr}');
+          var data = {jsonstr};
           document.querySelector("#{html_id}").data = data;
         </script>"""
     html = HTML_TEMPLATE.format(html_id=html_id, jsonstr=jsonstr)
