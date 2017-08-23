01c646abcb84195bafc26182d4ea7a07426d34e4
Rajiv Bharadwaja <4618540+rajivpb@users.noreply.github.com>
2017-08-23 11:05:37 -0700

    Introducing Datalab Notebook Productionization with %pipeline (#480)
    * Airflow python spec generation.
    
    * Airflow python spec generation
    
    * Deleting extraneous files from tests
    
    * Removing TODO to pick either Yaml or Json. We'll allow both. No harm in that.
    
    * Adding ability to specify 'query' instead of 'bql' for BigQuery.
    
    * airflow_tests were being run twice in error. Fixing.
    
    * Auto merged files after git rebase
    
    * Resolving test conflicts.
    
    * Fixing up test related changes
    
    * Making new top-level schedule section in YAML spec; AirflowDag->AirflowPipeline rename
    
    * dag_id -> pipeline_id
    
    * Introducing BigQueryTableDeleteOperator, BigQueryToBigQueryOperator, and BigQueryToCloudStorageOperator
    
    * _airflow_dag.py -> _airflow_pipeline.py
    
    * Introducing %pipeline magic with support for "create" command. Also, some refactoring: AirflowPipeline -> Pipeline. Pipeline -> Deploy.
    
    * Deleting airflow_job.py
    
    * Importing the %%pipeline magic to kernels so that it's callable from datalab.
    
    * Plugging into datalab object model to parse "pipeline create" magic.
    
    * Stub implementation for pipeline load functionality
    
    * BQ queries can now be referenced by name in the yaml spec!
    
    * Some refactoring / clean-up.
    
    * airflow -> pipeline
    
    * Using datalab's parse_config for cell_body parsing (and throwing out _coders.py)
    
    * Airflow spec now does not specify default params if the values are defaults.
    
    * Removing "deploy" subcommand. This will be folded into create and there will be a single subcommand under the %pipeline magic.
    
    * Some cleanup ahead of PR.
    
    * Minor cleanup
    
    * Adding doc strings, and fixing tests because python 3 renamed dict.iteritems -> dict.items
    
    * More doc-strings
    
    * Sorting dictionaries after iteration so that the resulting order is stable.
    
    * Fixing lint issues
    
    * Fixing flake8 errors (still a few to go).
    
    * Fixing/suppressing remaining flake8 errors
    
    * Fixing year (2015->2017) in all __init__.py files
    
    * Responding to code-review suggestions

--- setup.py
+++ setup.py
@@ -29,6 +29,8 @@ setup(
     'google.datalab.kernel',
     'google.datalab.ml',
     'google.datalab.notebook',
+    'google.datalab.pipeline',
+    'google.datalab.pipeline.commands',
     'google.datalab.stackdriver',
     'google.datalab.stackdriver.commands',
     'google.datalab.stackdriver.monitoring',
---
+++ tests/kernel/pipeline_tests.py
@@ -0,0 +1,180 @@
+# Copyright 2017 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing permissions and limitations under
+# the License.
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+from oauth2client.client import AccessTokenCredentials
+
+import google.datalab.bigquery
+import google.datalab.pipeline.commands._pipeline
+import google.datalab.utils.commands
+import mock
+import unittest
+
+# import Python so we can mock the parts we need to here.
+import IPython
+import IPython.core.magic
+
+
+def noop_decorator(func):
+  return func
+
+
+IPython.core.magic.register_line_cell_magic = noop_decorator
+IPython.core.magic.register_line_magic = noop_decorator
+IPython.core.magic.register_cell_magic = noop_decorator
+IPython.get_ipython = mock.Mock()
+
+
+class TestCases(unittest.TestCase):
+
+  @staticmethod
+  def _create_context():
+    project_id = 'test'
+    creds = AccessTokenCredentials('test_token', 'test_ua')
+    return google.datalab.Context(project_id, creds)
+
+  @mock.patch('google.datalab.utils.commands.notebook_environment')
+  @mock.patch('google.datalab.Context.default')
+  def test_create_cell_no_name(self, mock_default_context,
+                               mock_notebook_environment):
+    env = {}
+    mock_default_context.return_value = TestCases._create_context()
+    mock_notebook_environment.return_value = env
+    IPython.get_ipython().user_ns = env
+
+    # test pipeline creation
+    p_body = 'foo'
+
+    # no pipeline name specified. should execute
+    with self.assertRaises(Exception):
+      google.datalab.pipeline.commands._pipeline._create_cell({'name': None},
+                                                              p_body)
+
+  @mock.patch('google.datalab.utils.commands.notebook_environment')
+  @mock.patch('google.datalab.Context.default')
+  def test_create_cell(self, mock_default_context, mock_notebook_environment):
+    env = {}
+    mock_default_context.return_value = TestCases._create_context()
+    mock_notebook_environment.return_value = env
+    IPython.get_ipython().user_ns = env
+
+    # test pipeline creation
+    p_body = """
+email: foo@bar.com
+schedule:
+  start_date: Jun 1 2005  1:33PM
+  end_date: Jun 10 2005  1:33PM
+  datetime_format: '%b %d %Y %I:%M%p'
+  schedule_interval: '@hourly'
+tasks:
+  print_pdt_date:
+    type: bash
+    bash_command: date
+  print_utc_date:
+    type: bash
+    bash_command: date -u
+    up_stream:
+      - print_pdt_date
+"""
+    # test pipeline creation
+    google.datalab.pipeline.commands._pipeline._create_cell({'name': 'p1'},
+                                                            p_body)
+
+    p1 = env['p1']
+    self.assertIsNotNone(p1)
+    self.assertEqual(p_body, p1._spec_str)
+    self.assertEqual(p1.py, """
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+from airflow.contrib.operators.bigquery_table_delete_operator import BigQueryTableDeleteOperator
+from airflow.contrib.operators.bigquery_to_bigquery import BigQueryToBigQueryOperator
+from airflow.contrib.operators.bigquery_to_gcs import BigQueryToCloudStorageOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'Datalab',
+    'depends_on_past': False,
+    'email': ['foo@bar.com'],
+    'start_date': datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p'),
+    'end_date': datetime.strptime('Jun 10 2005  1:33PM', '%b %d %Y %I:%M%p'),
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
+
+dag = DAG(dag_id='p1', schedule_interval='@hourly', default_args=default_args)
+
+print_pdt_date = BashOperator(task_id='print_pdt_date_id', bash_command='date', dag=dag)
+print_utc_date = BashOperator(task_id='print_utc_date_id', bash_command='date -u', dag=dag)
+print_utc_date.set_upstream(print_pdt_date)
+""")
+
+  @mock.patch('google.datalab.utils.commands.notebook_environment')
+  @mock.patch('google.datalab.Context.default')
+  def test_create_cell_with_variable(self, mock_default_context,
+                                     mock_notebook_environment):
+    mock_default_context.return_value = TestCases._create_context()
+    env = {}
+    env['foo_query'] = google.datalab.bigquery.Query(
+        'SELECT * FROM publicdata.samples.wikipedia LIMIT 5')
+    mock_notebook_environment.return_value = env
+    # TODO(rajivpb): Possibly not necessary
+    IPython.get_ipython().user_ns = env
+
+    # test pipeline creation
+    p_body = """
+email: foo@bar.com
+schedule:
+  start_date: Jun 1 2005  1:33PM
+  end_date: Jun 10 2005  1:33PM
+  datetime_format: '%b %d %Y %I:%M%p'
+  schedule_interval: '@hourly'
+tasks:
+  print_pdt_date:
+    type: bq
+    query: $foo_query
+"""
+
+    # no pipeline name specified. should execute
+    google.datalab.pipeline.commands._pipeline._create_cell({'name': 'p1'}, p_body)
+    p1 = env['p1']
+    self.assertIsNotNone(p1)
+    self.assertEqual(p_body, p1._spec_str)
+    self.assertEqual(p1.py, """
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+from airflow.contrib.operators.bigquery_table_delete_operator import BigQueryTableDeleteOperator
+from airflow.contrib.operators.bigquery_to_bigquery import BigQueryToBigQueryOperator
+from airflow.contrib.operators.bigquery_to_gcs import BigQueryToCloudStorageOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'Datalab',
+    'depends_on_past': False,
+    'email': ['foo@bar.com'],
+    'start_date': datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p'),
+    'end_date': datetime.strptime('Jun 10 2005  1:33PM', '%b %d %Y %I:%M%p'),
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
+
+dag = DAG(dag_id='p1', schedule_interval='@hourly', default_args=default_args)
+
+print_pdt_date = BigQueryOperator(task_id='print_pdt_date_id', bql='SELECT * FROM publicdata.samples.wikipedia LIMIT 5', use_legacy_sql=False, dag=dag)
+"""  # noqa
+    )
