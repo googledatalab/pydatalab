7df5646f69bbbf8672597dabbae5d03992d63cb2
Bradley Jiang <qimingj@google.com>
2017-09-04 16:03:55 -0700

    Update comments.

--- tests/mlworkbench_magic/explainer_tests.py
+++ tests/mlworkbench_magic/explainer_tests.py
@@ -9,7 +9,7 @@
 # is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 # or implied. See the License for the specific language governing permissions and limitations under
 # the License.
-"""Tests the \%\%ml magics functions without runing any jobs."""
+"""Integration Tests of PredictionExplainer."""
 
 from __future__ import absolute_import
 from __future__ import unicode_literals
