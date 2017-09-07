4aed0aa837e1d85de53e5347f6fe6369aa1d3ffe
Rajiv Bharadwaja <rajivpb@google.com>
2017-09-07 14:12:36 -0700

    Pinning pandas due to airflow version issues

--- setup.py
+++ setup.py
@@ -98,7 +98,11 @@ for accessing Google's Cloud Platform services such as Google BigQuery.
     'plotly==1.12.5',
     'httplib2==0.10.3',
     'oauth2client==2.2.0',
-    'pandas>=0.17.1',
+    # TODO(rajivpb): Pinning pandas to 0.19.1 is a temporary work-around for
+    # https://issues.apache.org/jira/browse/AIRFLOW-1179, because the version of airflow on PyPI
+    # does not seem to have the fix. We should unpin after installing a version of airflow that's
+    # built from HEAD.
+    'pandas==0.19.1',
     'pandas-profiling>=1.0.0a2',
     'python-dateutil==2.5.0',
     'pytz>=2015.4',
