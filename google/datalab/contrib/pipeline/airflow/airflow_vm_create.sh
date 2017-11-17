bb147e42ed33773c4c8581f7dcefbf6a62955ab3
Rajiv Bharadwaja <rajivpb@google.com>
2017-11-16 16:40:31 -0800

    Hard-code bucket and dag-path. Cleanup and parameterize script.

--- google/datalab/contrib/pipeline/airflow/airflow_vm_create.sh
+++ google/datalab/contrib/pipeline/airflow/airflow_vm_create.sh
@@ -1,30 +1,36 @@
 #!/usr/bin/env bash
-VM_NAME="instance-12"
-PROJECT_ID="cloud-ml-dev"
-ZONE="us-central1-b"
+VM_NAME=$1
+PROJECT_ID=$2
+ZONE=us-central1-b
 
-# TODO(rajivpb): These should have more official values
-AIRFLOW_VM_STARTUP_SCRIPT="gs://rajivpb-airflow-testing/airflow_vm_startup.sh"
-GCS_DAG_BUCKET_FOR_AIRFLOW="rajivpb-airflow-testing"
-GCS_DAG_PATH_FOR_AIRFLOW="/dags"
+$GSUTIL=gsutil
+$GCLOUD=gcloud
+
+SRC_BUCKET=rajivpb-airflow-testing
+GCS_DAG_BUCKET=$PROJECT_ID-datalab-airflow
+$GSUTIL mb gs://$GCS_DAG_BUCKET
+
+AIRFLOW_VM_STARTUP_SCRIPT_SRC=gs://$SRC_BUCKET/airflow_vm_startup.sh
+AIRFLOW_VM_STARTUP_SCRIPT=gs://$GCS_DAG_BUCKET/airflow_vm_startup.sh
+$GSUTIL cp $AIRFLOW_VM_STARTUP_SCRIPT_SRC $AIRFLOW_VM_STARTUP_SCRIPT
+
+DATALAB_TAR=datalab-1.1.0.tar
+$GSUTIL cp gs://$SRC_BUCKET/$DATALAB_TAR gs://$GCS_DAG_BUCKET/$DATALAB_TAR
 
 # The default values here were grabbed from Pantheon's "command-line" button for creating a VM
-gcloud beta compute --project $PROJECT_ID instances create $VM_NAME \
---zone $ZONE \
---machine-type "n1-standard-1" \
---network "default" \
---maintenance-policy "MIGRATE" \
---scopes "https://www.googleapis.com/auth/cloud-platform" \
---min-cpu-platform "Automatic" \
---image "debian-9-stretch-v20171025" \
---image-project "debian-cloud" \
---boot-disk-size "10" \
---boot-disk-type "pd-standard" \
---boot-disk-device-name $VM_NAME \
---metadata startup-script-url=$AIRFLOW_VM_STARTUP_SCRIPT \
-
-# TODO(rajivpb): Eventually, we need to set this metadata so that they can be parameters
-# --metadata GCS_DAG_BUCKET=$GCS_DAG_BUCKET_FOR_AIRFLOW,GCS_DAG_PATH=$GCS_DAG_PATH_FOR_AIRFLOW \
+$GCLOUD beta compute --project $PROJECT_ID instances create $VM_NAME \
+    --zone $ZONE \
+    --machine-type "n1-standard-1" \
+    --network "default" \
+    --maintenance-policy "MIGRATE" \
+    --scopes "https://www.googleapis.com/auth/cloud-platform" \
+    --min-cpu-platform "Automatic" \
+    --image "debian-9-stretch-v20171025" \
+    --image-project "debian-cloud" \
+    --boot-disk-size "10" \
+    --boot-disk-type "pd-standard" \
+    --boot-disk-device-name $VM_NAME \
+    --metadata startup-script-url=$AIRFLOW_VM_STARTUP_SCRIPT \
 
 sleep 30
-gcloud compute --project $PROJECT_ID ssh --zone $ZONE $VM_NAME
+$GCLOUD compute --project $PROJECT_ID ssh --zone $ZONE $VM_NAME
--- google/datalab/contrib/pipeline/airflow/airflow_vm_startup.sh
+++ google/datalab/contrib/pipeline/airflow/airflow_vm_startup.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
-AIRFLOW_CRON=temp_crontab.txt
-
 export AIRFLOW_HOME=airflow
-LOCAL_DAG_PATH=$AIRFLOW_HOME/dags
+PROJECT_ID=cloud-ml-dev
+GCS_DAG_BUCKET=$PROJECT_ID-datalab-airflow
+DAG_PATH="/dags"
 
-# TODO(rajivpb): These parameters should be gettable from the environment, i.e. they should have
-# been set by the VM's create script. So eventually, remove these lines.
-GCS_DAG_BUCKET="rajivpb-airflow-testing"
-GCS_DAG_PATH="/dags"
+LOCAL_DAG_PATH=$AIRFLOW_HOME$DAG_PATH
 
 apt-get --assume-yes install python-pip
-
 pip install airflow
-.local/bin/airflow initdb
+airflow initdb
 
-# TODO(rajivpb): Replace this with pip install datalab
+# TODO(rajivpb): Replace this with 'pip install datalab'
 DATALAB_TAR=datalab-1.1.0.tar
-gsutil cp gs://rajivpb-airflow-testing/$DATALAB_TAR $DATALAB_TAR
+gsutil cp gs:///$GCS_DAG_BUCKET/$DATALAB_TAR $DATALAB_TAR
 pip install $DATALAB_TAR
 rm $DATALAB_TAR
 
 # We append a gsutil rsync command to the cron file and have this run every minute to sync dags.
+AIRFLOW_CRON=temp_crontab.txt
 mkdir -p $LOCAL_DAG_PATH
 crontab -l > $AIRFLOW_CRON
-echo "* * * * * /usr/bin/gsutil rsync gs://$GCS_DAG_BUCKET/$GCS_DAG_PATH $LOCAL_DAG_PATH" >> $AIRFLOW_CRON
+echo "* * * * * gsutil rsync gs://$GCS_DAG_BUCKET/$DAG_PATH $LOCAL_DAG_PATH" >> $AIRFLOW_CRON
 crontab $AIRFLOW_CRON
 rm $AIRFLOW_CRON
