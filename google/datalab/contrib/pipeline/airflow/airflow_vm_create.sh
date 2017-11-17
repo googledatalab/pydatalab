eb2d895d8301707a7f4d8afa14e4591d1e684bd2
Rajiv Bharadwaja <rajivpb@google.com>
2017-11-16 19:33:05 -0800

    Simplifying release scripts

--- google/datalab/contrib/pipeline/airflow/airflow_vm_create.sh
+++ google/datalab/contrib/pipeline/airflow/airflow_vm_create.sh
@@ -3,26 +3,15 @@ PROJECT_ID=${1:-cloud-ml-dev}
 VM_NAME=${2:-instance-16}
 ZONE=us-central1-b
 
-GSUTIL=gsutil
-GCLOUD=gcloud
-
 # Make bucket
 GCS_DAG_BUCKET=$PROJECT_ID-datalab-airflow
-$GSUTIL mb gs://$GCS_DAG_BUCKET
+gsutil mb gs://$GCS_DAG_BUCKET
 
-# Copy startup script to the new bucket
-SRC_BUCKET=rajivpb-airflow-testing
-AIRFLOW_VM_STARTUP_SCRIPT_SRC=gs://$SRC_BUCKET/airflow_vm_startup.sh
-AIRFLOW_VM_STARTUP_SCRIPT=gs://$GCS_DAG_BUCKET/airflow_vm_startup.sh
-$GSUTIL cp $AIRFLOW_VM_STARTUP_SCRIPT_SRC $AIRFLOW_VM_STARTUP_SCRIPT
-
-# Copy datalab.tar to the new bucket
-# TODO(rajivpb): This should go away after we release pydatalab and pip install it from PyPI
-DATALAB_TAR=datalab-1.1.0.tar
-$GSUTIL cp gs://$SRC_BUCKET/$DATALAB_TAR gs://$GCS_DAG_BUCKET/$DATALAB_TAR
+DATALAB_PIPELINES_BUCKET=datalab-pipelines
+AIRFLOW_VM_STARTUP_SCRIPT=gs://$DATALAB_PIPELINES_BUCKET/airflow_vm_startup.sh
 
 # The default values here were grabbed from Pantheon's "command-line" button for creating a VM
-$GCLOUD beta compute --project $PROJECT_ID instances create $VM_NAME \
+gcloud beta compute --project $PROJECT_ID instances create $VM_NAME \
     --zone $ZONE \
     --machine-type "n1-standard-1" \
     --network "default" \
@@ -38,4 +27,4 @@ $GCLOUD beta compute --project $PROJECT_ID instances create $VM_NAME \
 
 # TODO(rajivpb): To be deleted; left here only for convenience
 sleep 30
-$GCLOUD compute --project $PROJECT_ID ssh --zone $ZONE $VM_NAME
+gcloud compute --project $PROJECT_ID ssh --zone $ZONE $VM_NAME
--- google/datalab/contrib/pipeline/airflow/airflow_vm_startup.sh
+++ google/datalab/contrib/pipeline/airflow/airflow_vm_startup.sh
@@ -3,16 +3,13 @@ export AIRFLOW_HOME=/etc/airflow
 PROJECT_ID=cloud-ml-dev
 GCS_DAG_BUCKET=$PROJECT_ID-datalab-airflow
 
-GSUTIL=gsutil
-GCLOUD=gcloud
-
 apt-get --assume-yes install python-pip
 pip install airflow
 airflow initdb
 
 # TODO(rajivpb): Replace this with 'pip install datalab'
 DATALAB_TAR=datalab-1.1.0.tar
-$GSUTIL cp gs://$GCS_DAG_BUCKET/$DATALAB_TAR $DATALAB_TAR
+gsutil cp gs://$GCS_DAG_BUCKET/$DATALAB_TAR $DATALAB_TAR
 pip install $DATALAB_TAR
 rm $DATALAB_TAR
 
@@ -22,11 +19,10 @@ crontab -l > $AIRFLOW_CRON
 DAG_FOLDER="dags"
 LOCAL_DAG_PATH=$AIRFLOW_HOME/$DAG_FOLDER
 mkdir -p $LOCAL_DAG_PATH
-
-# TODO(rajivpb): Temporarily give other users permissions to peek into the dag folder for debugging
-chmod a+rwx $AIRFLOW_HOME
-chmod a+rwx $LOCAL_DAG_PATH
-
 echo "* * * * * gsutil rsync gs://$GCS_DAG_BUCKET/$DAG_FOLDER $LOCAL_DAG_PATH" >> $AIRFLOW_CRON
 crontab $AIRFLOW_CRON
 rm $AIRFLOW_CRON
+
+# TODO(rajivpb): Delete these; just temporarily giving permissions for debugging
+chmod a+rwx $AIRFLOW_HOME
+chmod a+rwx $LOCAL_DAG_PATH
