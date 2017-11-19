201361f4cf71906d0274167627c82909877a0a9c
Rajiv Bharadwaja <rajivpb@google.com>
2017-11-19 00:51:23 -0800

    Script refactoring and adding comments. Should not result in any functional changes. Upping instance count.

--- google/datalab/contrib/pipeline/airflow/airflow_vm_startup.sh
+++ google/datalab/contrib/pipeline/airflow/airflow_vm_startup.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-PROJECT_ID=cloud-ml-dev
 export AIRFLOW_HOME=/airflow
 export AIRFLOW__CORE__DAGS_ARE_PAUSED_AT_CREATION=False
-AIRFLOW__CORE__LOAD_EXAMPLES=False
-GCS_DAG_BUCKET=$PROJECT_ID-datalab-airflow
+export AIRFLOW__CORE__LOAD_EXAMPLES=False
+mkdir $AIRFLOW_HOME
+# TODO(rajivpb): Probably not necessary to give such blanket access here.
+chmod a+rwx $AIRFLOW_HOME
 
 apt-get --assume-yes install python-pip
-
-mkdir $AIRFLOW_HOME
-chmod a+rwx $AIRFLOW_HOME
 
 # TODO(rajivpb): Replace this with 'pip install datalab'
 DATALAB_TAR=datalab-1.1.0.tar
@@ -16,15 +14,20 @@ gsutil cp gs://datalab-pipelines/$DATALAB_TAR $DATALAB_TAR
 pip install $DATALAB_TAR
 rm $DATALAB_TAR
 
+# Airflow is installed by datalab. So any airflow commands should follow the datalab install.
 airflow initdb
 airflow scheduler &
 
 # We append a gsutil rsync command to the cron file and have this run every minute to sync dags.
+# TODO(rajivpb): We should get this from the VM (gcloud or somehow)
+PROJECT_ID=cloud-ml-dev
+GCS_DAG_BUCKET=$PROJECT_ID-datalab-airflow
 AIRFLOW_CRON=temp_crontab.txt
 crontab -l > $AIRFLOW_CRON
 DAG_FOLDER="dags"
 LOCAL_DAG_PATH=$AIRFLOW_HOME/$DAG_FOLDER
 mkdir -p $LOCAL_DAG_PATH
+# TODO(rajivpb): Probably not necessary to give such blanket access here.
 chmod a+rwx $LOCAL_DAG_PATH
 echo "* * * * * gsutil rsync gs://$GCS_DAG_BUCKET/$DAG_FOLDER $LOCAL_DAG_PATH" >> $AIRFLOW_CRON
 crontab $AIRFLOW_CRON
