47bdee6e34af982021c83419eb48a08d234978e8
Rajiv Bharadwaja <rajivpb@google.com>
2017-12-18 17:19:26 -0800

    (Incomplete) Failing golden test

--- tests/bigquery/pipeline_tests.py
+++ tests/bigquery/pipeline_tests.py
@@ -12,7 +12,6 @@
 # or implied. See the License for the specific language governing permissions and limitations under
 # the License.
 
-import datetime
 import google
 import google.auth
 import google.datalab.contrib.bigquery.commands._bigquery as bq
