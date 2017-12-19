4172d64eef05a1e5c2c5d138270480158aac07a9
Rajiv Bharadwaja <rajivpb@google.com>
2017-12-18 18:26:42 -0800

    Fix flake8 errory

--- google/datalab/bigquery/_query.py
+++ google/datalab/bigquery/_query.py
@@ -368,7 +368,6 @@ class Query(object):
     return parsed_params
 
   @staticmethod
-
   def resolve_parameters(value, parameters, date_time=datetime.datetime.now(), macros=False):
     """ Resolve a format modifier with the corresponding value.
 
